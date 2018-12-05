23775ef8de89376981ccf4c88155362e689cdcf7
notedit <notedit@gmail.com>
2018-12-06 02:16:53 +0800

    fix test

--- gstreamer_test.go
+++ gstreamer_test.go
@@ -3,49 +3,50 @@ package gstreamer
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
-// func TestPipeline(t *testing.T) {
+func TestPipeline(t *testing.T) {
 
-// 	pipeline, err := New("videotestsrc  ! capsfilter name=filter ! autovideosink")
-// 	if err != nil {
-// 		t.Error("pipeline create error", err)
-// 		t.FailNow()
-// 	}
+	pipeline, err := New("videotestsrc  ! capsfilter name=filter ! autovideosink")
+	if err != nil {
+		t.Error("pipeline create error", err)
+		t.FailNow()
+	}
 
-// 	filter := pipeline.FindElement("filter")
+	filter := pipeline.FindElement("filter")
 
-// 	if filter == nil {
-// 		t.Error("pipeline find element error ")
-// 	}
+	if filter == nil {
+		t.Error("pipeline find element error ")
+	}
 
-// 	filter.SetCap("video/x-raw,width=1280,height=720")
+	filter.SetCap("video/x-raw,width=1280,height=720")
 
-// 	pipeline.Start()
+	pipeline.Start()
 
-// 	select {}
-// }
+	select {}
+}
 
-// func TestAppsrc(t *testing.T) {
+func TestAppsrc(t *testing.T) {
 
-// 	pipeline, err := New("appsrc name=mysource format=time is-live=true do-timestamp=true ! videoconvert ! autovideosink")
+	pipeline, err := New("appsrc name=mysource format=time is-live=true do-timestamp=true ! videoconvert ! autovideosink")
 
-// 	if err != nil {
-// 		t.Error("pipeline create error", err)
-// 		t.FailNow()
-// 	}
+	if err != nil {
+		t.Error("pipeline create error", err)
+		t.FailNow()
+	}
 
-// 	appsrc := pipeline.FindElement("mysource")
+	appsrc := pipeline.FindElement("mysource")
 
-// 	appsrc.SetCap("video/x-raw,format=RGB,width=320,height=240,bpp=24,depth=24")
+	appsrc.SetCap("video/x-raw,format=RGB,width=320,height=240,bpp=24,depth=24")
 
-// 	pipeline.Start()
+	pipeline.Start()
 
-// 	for {
-// 		time.Sleep(1 * time.Second)
-// 		appsrc.Push(make([]byte, 320*240*3))
-// 	}
-// }
+	for {
+		time.Sleep(1 * time.Second)
+		appsrc.Push(make([]byte, 320*240*3))
+	}
+}
 
 func TestAppsink(t *testing.T) {
 
